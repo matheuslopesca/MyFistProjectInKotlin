bba5222d65d01219a98ce94c16d9fff5bddbe857
Matheus Carvalho <matheuslopescarvalho@gmail.com>
2025-12-30 16:26:01 -0300

    feat: Add initial Kotlin class examples for account management, constructors, overloading, visibility, and exercises for random character generation and person activity.

---
+++ src/main/kotlin/classes/App6AccountVisibility.kt
@@ -0,0 +1,19 @@
+package main.kotlin.classes
+
+fun main() {
+    val a1 = Account("1234-5", "João")
+    val a2 = Account("4444-5", "Maria")
+    val a3 = Account()
+    val a4 = Account("8888-7", "Carlos", 10000.0)
+
+    a1.deposit(2000.0)
+    a2.deposit(150.0)
+    a4.deposit(2000)
+
+    a1.print()
+    a2.print()
+    a3.print()
+    a4.print()
+
+
+}
---
+++ src/main/kotlin/exercises/15ExRandom.kt
@@ -0,0 +1,26 @@
+package main.kotlin.exercises
+
+import kotlin.math.absoluteValue
+import kotlin.math.max
+import kotlin.random.Random
+
+fun main(){
+/*
+    print("Digite o Primeiro valor do range:")
+    val v1 = readln()
+    print("Digite o Segundo valor do range:")
+    val v2 = readln()
+*/
+    val random = CharGenerator('A', 'Z')
+
+    println("O Caractere gerado é ${random.next()}")
+}
+
+class CharGenerator(val min: Char, val max: Char) {
+/*
+    fun next(): Char {
+        return (min .. max).random()
+    }
+*/
+    fun next() = (min .. max).random()
+}
